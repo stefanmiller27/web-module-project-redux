6b28d62946af7a9ffb5492bf9e99b3531a79f315
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-01-21 08:54:01 -0500

    final code

--- src/actions/favoritesActions.js
+++ src/actions/favoritesActions.js
@@ -7,9 +7,11 @@ export const toggleFavorites = ()=> {
 }
 
 export const addFavorite = (movie)=> {
+
     return({type: ADD_FAVORITE, payload:movie});
 }
 
 export const removeFavorite = (id)=> {
     return({type: REMOVE_FAVORITE, payload:id});
 }
+
